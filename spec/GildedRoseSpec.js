355552ec883144f8479ad7f9a77e1eb5fae1a029
danroche10 <danielroche10@gmail.com>
2021-10-01 14:39:32 +0100

    removed unnecesary files and changed file names

--- spec/GildedRoseSpec.js
+++ spec/GildedRoseSpec.js
@@ -1,7 +1,8 @@
-var { Shop, Item } = require('../src/gilded_rose.js');
+const { Shop, Item } = require('../src/GildedRose.js');
 describe('Gilded Rose', function () {
   it('should foo', function () {
     const gildedRose = new Shop([new Item('foo', 2, 2)]);
+
     expect(gildedRose.updateQuality()[0]).toEqual(
       jasmine.objectContaining({
         name: 'foo',
