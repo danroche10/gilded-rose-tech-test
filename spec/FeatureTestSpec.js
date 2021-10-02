cb0bb18c5a4dedd017be25b65919c10a3c4e15d9
danroche10 <danielroche10@gmail.com>
2021-10-02 12:40:47 +0100

    updateStock method returns items with correct sellIn and quality after being called twice. Test passing

--- spec/FeatureTestSpec.js
+++ spec/FeatureTestSpec.js
@@ -13,13 +13,16 @@ fdescribe('FeatureTest', function () {
   let gildedRose;
 
   describe('one of each item', function () {
-    brieItem = new Brie('Aged Brie', 10, 10);
-    backStagePass = new BackStagePass('Music Concert', 10, 10);
-    standardItem = new StandardItem('Pears', 10, 10);
-    sulfura = new Sulfura('sulfura', 'n/a', 10);
-    items = [brieItem, backStagePass, standardItem, sulfura];
-    gildedRose = new Shop(items);
-    it('should increase quality by 1 if quality is less than 50', function () {
+    beforeEach(function () {
+      brieItem = new Brie('Aged Brie', 10, 10);
+      backStagePass = new BackStagePass('Music Concert', 10, 10);
+      standardItem = new StandardItem('Pears', 10, 10);
+      sulfura = new Sulfura('sulfura', 'n/a', 10);
+      items = [brieItem, backStagePass, standardItem, sulfura];
+      gildedRose = new Shop(items);
+    });
+
+    it('returns items with correct sellIn and quality after being called once', function () {
       gildedRose.updateStock();
       expect(brieItem).toEqual(
         jasmine.objectContaining({
@@ -37,6 +40,35 @@ fdescribe('FeatureTest', function () {
         jasmine.objectContaining({
           sellIn: 9,
           quality: 9,
+        })
+      );
+      expect(sulfura).toEqual(
+        jasmine.objectContaining({
+          sellIn: 'n/a',
+          quality: 10,
+        })
+      );
+    });
+
+    it('returns items with correct sellIn and quality after being called twice', function () {
+      gildedRose.updateStock();
+      gildedRose.updateStock();
+      expect(brieItem).toEqual(
+        jasmine.objectContaining({
+          sellIn: 8,
+          quality: 12,
+        })
+      );
+      expect(backStagePass).toEqual(
+        jasmine.objectContaining({
+          sellIn: 8,
+          quality: 14,
+        })
+      );
+      expect(standardItem).toEqual(
+        jasmine.objectContaining({
+          sellIn: 8,
+          quality: 8,
         })
       );
       expect(sulfura).toEqual(
