cad43dce851b807d085e1bc343f472cd43a31a62
danroche10 <danielroche10@gmail.com>
2021-10-02 16:33:50 +0100

    conjuredItem quality doesn't drop below zero and quality decreases twice as fast when sellIn below zero with tests passing

--- spec/ConjuredItemSpec.js
+++ spec/ConjuredItemSpec.js
@@ -14,6 +14,30 @@ describe('ConjuredItem', function () {
       );
     });
 
+    it('quality should not reduce below 0', function () {
+      conjuredItem = new ConjuredItem('ConjuredItem', 8, 0);
+
+      expect(conjuredItem.updateItem()).toEqual(
+        jasmine.objectContaining({
+          name: 'ConjuredItem',
+          sellIn: 7,
+          quality: 0,
+        })
+      );
+    });
+
+    it('quality should degrade twice as fast once sell by date has past', function () {
+      conjuredItem = new ConjuredItem('Conjured Item', -4, 10);
+
+      expect(conjuredItem.updateItem()).toEqual(
+        jasmine.objectContaining({
+          name: 'Conjured Item',
+          sellIn: -5,
+          quality: 6,
+        })
+      );
+    });
+
     it('should throw error if negative quality has been inputted', function () {
       conjuredItem = new ConjuredItem('Conjured Item', 10, -4);
 
